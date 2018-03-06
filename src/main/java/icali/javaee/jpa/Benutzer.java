5901fdb9a417c4045788f0e3af002158825f6f6f
Patrick Baumer <Patrick Baumer@172.16.62.69>
2018-03-06 13:39:12 +0100

    Termin-Entities (erstellt) Kalender-Entities (erstellt) Benutzer-Entities (überarbeitet)

--- src/main/java/icali/javaee/jpa/Benutzer.java
+++ src/main/java/icali/javaee/jpa/Benutzer.java
@@ -21,6 +21,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.TableGenerator;
 import javax.persistence.Transient;
@@ -48,6 +49,13 @@ public class Benutzer implements Serializable{
     @NotNull(message = "Der Benutzername darf nicht leer sein.")
     private String username;
     
+    @ManyToMany(mappedBy = "benutzerList")
+    List<Kalender> kalenderList = new ArrayList<>();
+    
+    @OneToMany
+    List<Termin> terminList = new ArrayList<>();
+    
+  
     public class Password {
         @Size(min = 6, max = 64, message = "Das Passwort muss zwischen sechs und 64 Zeichen lang sein.")
         public String password = "";
@@ -76,61 +84,7 @@ public class Benutzer implements Serializable{
     @Pattern(regexp = "(\\w|\\W)*@(\\w|\\W)*.(\\w|\\W){1,4}", message ="Die E-Mail muss muster@muster.de")
     @NotNull (message = "E-Mail-Adresse darf nicht leer sein.")
     private String email;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getVunname() {
-        return vunname;
-    }
-
-    public void setVunname(String vunname) {
-        this.vunname = vunname;
-    }
-
-    //<editor-fold defaultstate="collapsed" desc="Konstruktoren">
-    public Benutzer() {
-    }
-
-    public Benutzer(String username, String password) {
-        this.username = username;
-        this.password.password = password;
-        this.passwordHash = this.hashPassword(password);
-    }
-    
-     public Benutzer(String username, String password, String vunname, String email) {
-        this.username = username;
-        this.password.password = password;
-        this.passwordHash = this.hashPassword(password);
-        this.vunname = vunname;
-        this.email = email;
-    }
-    //</editor-fold>
-
-    //<editor-fold defaultstate="collapsed" desc="Setter und Getter">
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String id) {
-        this.username = id;
-    }
-
-   
-    //</editor-fold>
-
-    //<editor-fold defaultstate="collapsed" desc="Passwort setzen und prüfen">
-    /**
-     * Berechnet der Hash-Wert zu einem Passwort.
-     *
-     * @param password Passwort
-     * @return Hash-Wert
-     */
+ 
     private String hashPassword(String password) {
         byte[] hash;
 
@@ -149,78 +103,18 @@ public class Benutzer implements Serializable{
         return bigInt.toString(16);
     }
 
-    /**
-     * Berechnet einen Hashwert aus dem übergebenen Passwort und legt ihn im
-     * Feld passwordHash ab. Somit wird das Passwort niemals als Klartext
-     * gespeichert.
-     * 
-     * Gleichzeitig wird das Passwort im nicht gespeicherten Feld password
-     * abgelegt, um durch die Bean Validation Annotationen überprüft werden
-     * zu können.
-     *
-     * @param password Neues Passwort
-     */
+
     public void setPassword(String password) {
         this.password.password = password;
         this.passwordHash = this.hashPassword(password);
     }
 
-    /**
-     * Nur für die Validierung bei einer Passwortänderung!
-     * @return Neues, beim Speichern gesetztes Passwort
-     */
+
     public Password getPassword() {
         return this.password;
     }
-    
-    /**
-     * Prüft, ob das übergebene Passwort korrekt ist.
-     *
-     * @param password Zu prüfendes Passwort
-     * @return true wenn das Passwort stimmt sonst false
-     */
+
     public boolean checkPassword(String password) {
         return this.passwordHash.equals(this.hashPassword(password));
     }
-    //</editor-fold>
-
-    //<editor-fold defaultstate="collapsed" desc="Zuordnung zu Benutzergruppen">
-    /**
-     * @return Eine unveränderliche Liste aller Benutzergruppen
-     */
-    
-//    PRÜFEN OB NOTWENDIG
-    public List<String> getGroups() {
-        List<String> groupsCopy = new ArrayList<>();
-
-        this.groups.forEach((groupname) -> {
-            groupsCopy.add(groupname);
-        });
-
-        return groupsCopy;
-    }
-
-    /**
-     * Fügt den Benutzer einer weiteren Benutzergruppe hinzu.
-     *
-     * @param groupname Name der Benutzergruppe
-     */
-    public void addToGroup(String groupname) {
-        if (!this.groups.contains(groupname)) {
-            this.groups.add(groupname);
-        }
-    }
-
-    /**
-     * Entfernt den Benutzer aus der übergebenen Benutzergruppe.
-     *
-     * @param groupname Name der Benutzergruppe
-     */
-    public void removeFromGroup(String groupname) {
-        this.groups.remove(groupname);
-    }
-    //</editor-fold>
-
 }
-
-
--- src/main/java/icali/javaee/jpa/Termin.java
+++ src/main/java/icali/javaee/jpa/Termin.java
@@ -5,10 +5,82 @@
  */
 package icali.javaee.jpa;
 
+import java.io.Serializable;
+import java.sql.Time;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.TableGenerator;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  *
  * @author Patrick Baumer
  */
-public class Termin {
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Termin implements Serializable{
+
+    @Id
+    @GeneratedValue(generator = "termin_id")
+    @TableGenerator(name = "termin_id", initialValue = 0)
+    private Long terminId;
+    
+    @Column(name = "t_name")
+    @NotNull(message = "Bitte geben Sie einen Terminnnamen ein")
+    private Time terminName;
+    
+    @Column(name = "t_ort")
+    @NotNull(message = "Bitte einen Ort angeben")
+    private Time terminOrt;
+        
+    @Column(name = "t_beschreibung")
+    @Lob
+    private String terminBeschreibung;
+    
+    @Column(name = "t_erinnerung")
+    private Boolean terminErinnerung;
+    
+    @Column(name = "t_start_uhrzeit")
+    @NotNull(message = "Bitte eine Anfangsuhrzeit angeben")
+    private Time startUhrzeit;
+    
+    @Column(name = "t_start_datum")
+    @NotNull(message = "Bitte eine Anfangsdatum angeben")
+    private Date startDatum;
+    
+    @Column (name = "t_ende_uhrzeit")
+    @NotNull(message = "Bitte eine Enduhrzeit angeben")
+    private Time endeUhrzeit;
+    
+    @Column(name = "t_ende_datum")
+    @NotNull(message = "Bitte eine Enddatum angeben")
+    private Date endeDatum;
+    
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    Kalender terminInKalender = null;
+    
+    @OneToOne
+    Benutzer ersteller = null;
+    
+    
+    
+
+    
     
 }
