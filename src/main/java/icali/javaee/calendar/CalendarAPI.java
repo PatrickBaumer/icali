44351a40ddf102083e9b6d11e52249079a28b1f8
x7 <Philipp.Delmonego@live.de>
2018-03-08 13:41:11 +0100

    monthRepresentation, weekRepresentation hinzugefügt, für LocalTime wurden noch keine Methoden implementiert

--- src/main/java/icali/javaee/calendar/CalendarAPI.java
+++ src/main/java/icali/javaee/calendar/CalendarAPI.java
@@ -5,10 +5,14 @@
  */
 package icali.javaee.calendar;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.Month;
 import java.time.temporal.WeekFields;
 import java.util.Locale;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  *
@@ -16,8 +20,8 @@ import java.util.Locale;
  */
 public class CalendarAPI {
     
-    LocalDate date;
-    LocalTime time;
+    private LocalDate date;
+    private LocalTime time;
     
     public CalendarAPI(){
         
@@ -40,6 +44,16 @@ public class CalendarAPI {
     
     public LocalTime getLocalTime(){
         return time;
+    }
+    
+    public int getWeekOfMonth(){
+        WeekFields weekFields = WeekFields.of(Locale.getDefault()); 
+        return date.get(weekFields.weekOfMonth());
+    }
+    
+    public int getWeekOfYear(){
+        WeekFields weekFields = WeekFields.of(Locale.getDefault()); 
+        return date.get(weekFields.weekOfYear());
     }
     
     // LocalDate manipulation
@@ -75,4 +89,54 @@ public class CalendarAPI {
         return date.getYear();
     }
     
+    // Ausgaben für Kalendardarstellung
+    /**
+     * 
+     * @param localDate
+     * @return weekMap -> eine Map mit den Tag des Monats al Key und dem Wochentag als Value
+     * mapping Tag & Wochentag
+     */
+    public Map<Integer, String> weekRepresentation(LocalDate localDate){
+        Map<Integer, String> weekMap = new TreeMap<>();
+        DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+        LocalDate monday;
+        switch (dayOfWeek) {  
+            case TUESDAY:monday = localDate.minusDays(1);
+              break;
+            case WEDNESDAY:monday = localDate.minusDays(2);
+              break;
+            case THURSDAY:monday = localDate.minusDays(3);
+              break;
+            case FRIDAY:monday = localDate.minusDays(4);
+              break;
+            case SATURDAY:monday = localDate.minusDays(5);
+              break;
+            case SUNDAY:monday = localDate.minusDays(6);
+              break;
+            default:monday=localDate;
+              break;
+        }
+        for(int i=0; i<7;i++){
+            LocalDate d = monday.plusDays(i);
+            weekMap.put(d.getDayOfMonth(), d.getDayOfWeek().name());
+        }
+        return weekMap; 
+    }
+    /**
+     * 
+     * @param localDate
+     * @return monthMap -> eine Map mit dem Tag des Monats als Key und dem Wochentag als Value
+     * mapping Tag & Wochentag
+     */
+    public Map<Integer, String> monthRepresentation(LocalDate localDate){
+        Map<Integer, String> monthMap = new TreeMap<>();
+        int month =localDate.getMonthValue();
+        int year = localDate.getYear();
+        for(int i =1; i<=localDate.lengthOfMonth(); i++){
+            LocalDate dayOfMonth = LocalDate.of(year, month, i);
+            monthMap.put(i, dayOfMonth.getDayOfWeek().name());
+        }
+        return monthMap; 
+    }
+
 }
