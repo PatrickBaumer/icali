be2bcb17aefdce96958d94223fa9f4166545b12b
x7 <Philipp.Delmonego@live.de>
2018-03-26 17:51:02 +0200

    mapping für Wochen & Termine, & Monat & Termine

--- src/main/java/icali/javaee/ejb/KalenderBean.java
+++ src/main/java/icali/javaee/ejb/KalenderBean.java
@@ -11,18 +11,16 @@ import icali.javaee.jpa.Termin;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.ZoneId;
 import java.time.temporal.WeekFields;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.TreeMap;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 
 /**
  *
@@ -30,93 +28,78 @@ import javax.persistence.criteria.Root;
  */
 @Stateless
 @RolesAllowed("icali-app-user")
-public class KalenderBean extends EntityBean<Kalender, Long>{
-    
+public class KalenderBean extends EntityBean<Kalender, Long> {
+
     private LocalDate date = LocalDate.now();
     private LocalTime time = LocalTime.now();
-    
+
     public KalenderBean() {
         super(Kalender.class);
     }
-    
-    public Map<LocalDate, Map<Kategorie, List<Termin>>> getTermine(List<Kategorie> kategorieList){
-        Map<LocalDate, Map<Kategorie, List<Termin>>> dateMap = new TreeMap<>();
-        Map<Kategorie, List<Termin>> kategorieMap = new HashMap<>();
-        for(Kategorie kategorie : kategorieList){
-            List<Termin> terminList = em.createQuery("SELECT k FROM Kalender k WHERE k.benutzerList.username = :username ORDER BY k.kalenderTitel")
-               .setParameter("kategorie", kategorie)
-               .getResultList();
-            kategorieMap.put(kategorie, terminList);
-        }
-        dateMap.put(date, kategorieMap);
-        return dateMap;
-    }
-    
-    public void getTermineByKalender(){}
-    
+
     public List<Kalender> findByUsername(String username) {
-       return em.createQuery("SELECT k FROM Kalender k WHERE k.benutzerList.username = :username ORDER BY k.kalenderTitel")
-               .setParameter("username", username)
-               .getResultList();
-   }
-   
-   public List<Kalender> findByKalenderId(Long KalenderId) {
-       return em.createQuery("SELECT k FROM Kalender k WHERE k.kalenderId = :kalenderId ORDER BY k.kalenderTitel")
-               .setParameter("KalenderId", KalenderId)
-               .getResultList();
-   }
-   
-   public List<Kalender> findByKalenderTitel(String kalenderTitel) {
-       return em.createQuery("SELECT k FROM Kalender k WHERE k.kalenderTitel = :kalender ORDER BY k.kalenderTitel")
-               .setParameter("kalenderTitel", kalenderTitel)
-               .getResultList();
-   }   
-   
-   public LocalDate getCurrentLocalDate() {
-       return LocalDate.now();
-   }
-   
-   public void setCurrentLocalDate(){
-       date = LocalDate.now();
-   }
-   
-   public LocalTime getCurrentLocalTime(){
+        return em.createQuery("SELECT k FROM Kalender k WHERE k.benutzerList.username = :username ORDER BY k.kalenderTitel")
+                .setParameter("username", username)
+                .getResultList();
+    }
+
+    public Kalender findByKalenderId(Long kalenderId) {
+        return (Kalender) em.createQuery("SELECT k FROM Kalender k WHERE k.kalenderId = :kalenderId ORDER BY k.kalenderTitel")
+                .setParameter("kalenderId", kalenderId)
+                .getSingleResult();
+    }
+
+    public List<Kalender> findByKalenderTitel(String kalenderTitel) {
+        return em.createQuery("SELECT k FROM Kalender k WHERE k.kalenderTitel = :kalender ORDER BY k.kalenderTitel")
+                .setParameter("kalenderTitel", kalenderTitel)
+                .getResultList();
+    }
+
+    public LocalDate getCurrentLocalDate() {
+        return LocalDate.now();
+    }
+
+    public void setCurrentLocalDate() {
+        date = LocalDate.now();
+    }
+
+    public LocalTime getCurrentLocalTime() {
         return LocalTime.now();
     }
-    
-    public LocalDate getLocalDate(){
+
+    public LocalDate getLocalDate() {
         return date;
     }
-    
-    public LocalTime getLocalTime(){
+
+    public LocalTime getLocalTime() {
         return time;
     }
-    
-    public int getWeekOfMonth(){
-        WeekFields weekFields = WeekFields.of(Locale.getDefault()); 
+
+    public int getWeekOfMonth() {
+        WeekFields weekFields = WeekFields.of(Locale.getDefault());
         return date.get(weekFields.weekOfMonth());
     }
-    
-    public int getWeekOfYear(){
-        WeekFields weekFields = WeekFields.of(Locale.getDefault()); 
+
+    public int getWeekOfYear() {
+        WeekFields weekFields = WeekFields.of(Locale.getDefault());
         return date.get(weekFields.weekOfYear());
     }
-    
-    public int nextWeek(){
+
+    public int nextWeek() {
         LocalDate hilf = date.plusWeeks(1);
-        WeekFields weekFields = WeekFields.of(Locale.getDefault()); 
+        WeekFields weekFields = WeekFields.of(Locale.getDefault());
         return hilf.get(weekFields.weekOfYear());
     }
-    
-    public int nextMonth(){
+
+    public int nextMonth() {
         LocalDate hilf = date.plusMonths(1);
         return hilf.getMonthValue();
     }
-    
-    public void nextMonthVoid(){
-        date=date.plusMonths(1);
+
+    public void nextMonthVoid() {
+        date = date.plusMonths(1);
     }
-    
+
     public String getMonthName() {
         String monthName = "";
         switch (date.getMonthValue()) {
@@ -157,108 +140,167 @@ public class KalenderBean extends EntityBean<Kalender, Long>{
                 monthName = "Dezember";
                 break;
             default:;
-                break;               
+                break;
         }
         return monthName;
     }
-    
+
     public int getYear() {
         return date.getYear();
     }
-    
-    public int nextYear(){
+
+    public int nextYear() {
         LocalDate hilf = date.plusYears(1);
         return hilf.getYear();
     }
-    
+
     public void nextYearVoid() {
         date = date.plusYears(1);
     }
-    
-    public int lastWeek(){
+
+    public int lastWeek() {
         LocalDate hilf = date.minusWeeks(1);
-        WeekFields weekFields = WeekFields.of(Locale.getDefault()); 
+        WeekFields weekFields = WeekFields.of(Locale.getDefault());
         return hilf.get(weekFields.weekOfYear());
     }
-    
-    public int lastMonth(){
+
+    public int lastMonth() {
         LocalDate hilf = date.minusMonths(1);
         return hilf.getMonthValue();
     }
-    
-    public void lastMonthVoid(){
+
+    public void lastMonthVoid() {
         date = date.minusMonths(1);
     }
-    
-    public int lastYear(){
+
+    public int lastYear() {
         LocalDate hilf = date.minusYears(1);
         return hilf.getYear();
     }
-    
+
     public void lastYearVoid() {
         date = date.minusYears(1);
     }
-    
+
+    // Mapping für die Terminausgabe
+    /**
+     * @param kalenderId
+     * @param localDate
+     * @return weekMap -> eine Map mit den Tag und eine Liste mit Terminen zu
+     * diesem Tag als Value mapping Tag & dazugehörigen Termine
+     *
+     */
+
+    public Map<Integer, List<Termin>> getWeekMapByKalenderId(Long kalenderId, LocalDate localDate) {
+        Map<Integer, List<Termin>> weekMap = new TreeMap<>();
+        for (int i = 0; i < 7; i++) {
+            List<Termin> terminByDayList = new ArrayList<>();
+            
+            for (Termin termin : findByKalenderId(kalenderId).getTerminList()) {
+                LocalDate startDatum = termin.getStartDatum().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                
+                if (getMonday(localDate).plusDays(i).isEqual(startDatum)) {
+                    terminByDayList.add(termin);
+                }
+            }
+            weekMap.put(getMonday(localDate).plusDays(i).getDayOfMonth(), terminByDayList);
+        }
+        return weekMap;
+    }
+
+    /**
+     * @param kalenderId
+     * @param localDate
+     * @return weekMap -> eine Map mit den Tag und eine Liste mit Terminen zu
+     * diesem Tag als Value mapping Tag & dazugehörigen Termine
+     *
+     */
+    public Map<Integer, List<Termin>> getMonthMapByKalenderId(Long kalenderId, LocalDate localDate) {
+        Map<Integer, List<Termin>> monthMap = new TreeMap<>();
+        for (int i = 1; i <= localDate.lengthOfMonth(); i++) {
+            List<Termin> terminByDayList = new ArrayList<>();
+            
+            for (Termin termin : findByKalenderId(kalenderId).getTerminList()) {
+                LocalDate startDatum = termin.getStartDatum().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                
+                if (getMonday(localDate).plusDays(i).isEqual(startDatum)) {
+                    terminByDayList.add(termin);
+                }
+            }
+            monthMap.put(i, terminByDayList);
+        }
+        return monthMap;
+    }
+
     // Ausgaben für Kalendardarstellung
     /**
-     * 
+     *
      * @param localDate
-     * @return weekMap -> eine Map mit den Tag des Monats al Key und dem Wochentag als Value
-     * mapping Tag & Wochentag
+     * @return weekMap -> eine Map mit den Tag des Monats als Key und dem
+     * Wochentag als Value mapping Tag & Wochentag
      */
-    public Map<Integer, String> weekRepresentation(LocalDate localDate){
+    public Map<Integer, String> weekRepresentation(LocalDate localDate) {
         Map<Integer, String> weekMap = new TreeMap<>();
-        DayOfWeek dayOfWeek = localDate.getDayOfWeek();
-        LocalDate monday;
-        switch (dayOfWeek) {  
-            case TUESDAY:monday = localDate.minusDays(1);
-              break;
-            case WEDNESDAY:monday = localDate.minusDays(2);
-              break;
-            case THURSDAY:monday = localDate.minusDays(3);
-              break;
-            case FRIDAY:monday = localDate.minusDays(4);
-              break;
-            case SATURDAY:monday = localDate.minusDays(5);
-              break;
-            case SUNDAY:monday = localDate.minusDays(6);
-              break;
-            default:monday=localDate;
-              break;
+
+        for (int i = 0; i < 7; i++) {
+            weekMap.put(getMonday(localDate).plusDays(i).getDayOfMonth(),
+                    getMonday(localDate).plusDays(i).getDayOfWeek().name());
         }
-        for(int i=0; i<7;i++){
-            LocalDate d = monday.plusDays(i);
-            weekMap.put(d.getDayOfMonth(), d.getDayOfWeek().name());
-        }
-        return weekMap; 
+        return weekMap;
     }
+
+    public LocalDate getMonday(LocalDate localDate) {
+        DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+
+        switch (dayOfWeek) {
+            case TUESDAY:
+                return localDate.minusDays(1);
+
+            case WEDNESDAY:
+                return localDate.minusDays(2);
+
+            case THURSDAY:
+                return localDate.minusDays(3);
+
+            case FRIDAY:
+                return localDate.minusDays(4);
+
+            case SATURDAY:
+                return localDate.minusDays(5);
+
+            case SUNDAY:
+                return localDate.minusDays(6);
+
+            default:
+                return localDate;
+        }
+    }
+
     /**
-     * 
+     *
      * @param localDate
-     * @return monthMap -> eine Map mit dem Tag des Monats als Key und dem Wochentag als Value
-     * mapping Tag & Wochentag
+     * @return monthMap -> eine Map mit dem Tag des Monats als Key und dem
+     * Wochentag als Value mapping Tag & Wochentag
      */
-    public Map<Integer, String> monthRepresentation(LocalDate localDate){
+    public Map<Integer, String> monthRepresentation(LocalDate localDate) {
         Map<Integer, String> monthMap = new TreeMap<>();
-        int month =localDate.getMonthValue();
-        for(int i =1; i<=localDate.lengthOfMonth(); i++){
-            LocalDate dayOfMonth = LocalDate.of(localDate.getYear(), month, i);
+        for (int i = 1; i <= localDate.lengthOfMonth(); i++) {
+            LocalDate dayOfMonth = LocalDate.of(localDate.getYear(), localDate.getMonthValue(), i);
             monthMap.put(i, dayOfMonth.getDayOfWeek().name());
         }
-        return monthMap; 
+        return monthMap;
     }
-    
-    public LocalDate[][] weeksInMonth(LocalDate localDate){
+
+    public LocalDate[][] weeksInMonth(LocalDate localDate) {
         Map<LocalDate, String> shownMonthsMap = shownMonthRepresentation(localDate.withDayOfMonth(1));
-        
+
         LocalDate[][] shownWeeks = new LocalDate[6][7];
-        
+
         int weekCounter = 0;
         int dayInWeekCounter = 0;
-        
-        
-        for(LocalDate iterationDate : shownMonthsMap.keySet()) {
-            if (dayInWeekCounter==7) {
+
+        for (LocalDate iterationDate : shownMonthsMap.keySet()) {
+            if (dayInWeekCounter == 7) {
                 weekCounter++;
                 dayInWeekCounter = 0;
             }
@@ -267,38 +309,18 @@ public class KalenderBean extends EntityBean<Kalender, Long>{
         }
         return shownWeeks;
     }
-    
+
     public Map<LocalDate, String> shownMonthRepresentation(LocalDate localDate) {
         Map<LocalDate, String> shownMonthsMap = new TreeMap<>();
-        DayOfWeek dayOfWeek = localDate.getDayOfWeek();
-        LocalDate monday;
-        
-        switch (dayOfWeek) {  
-            case TUESDAY:monday = localDate.minusDays(1);
-              break;
-            case WEDNESDAY:monday = localDate.minusDays(2);
-              break;
-            case THURSDAY:monday = localDate.minusDays(3);
-              break;
-            case FRIDAY:monday = localDate.minusDays(4);
-              break;
-            case SATURDAY:monday = localDate.minusDays(5);
-              break;
-            case SUNDAY:monday = localDate.minusDays(6);
-              break;
-            default:monday=localDate;
-              break;
-        }
-        
-        boolean loopBreaker= false;
-        
-        for(int i=0; loopBreaker==false; i++) {
-            LocalDate d = monday.plusDays(i);
+
+        boolean loopBreaker = false;
+        for (int i = 0; loopBreaker == false; i++) {
+            LocalDate d = getMonday(localDate).plusDays(i);
             shownMonthsMap.put(d, d.getDayOfWeek().name());
-            if (i>38 && d.getDayOfWeek().name().equals("SUNDAY")) {
+            if (i > 38 && d.getDayOfWeek().name().equals("SUNDAY")) {
                 loopBreaker = true;
             }
         }
         return shownMonthsMap;
-    }  
+    }
 }
